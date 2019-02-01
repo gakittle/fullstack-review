335f09d322eb2a97d9ebb61a9f160b66be18022f
Greg Kittleson <greg.kittleson@gmail.com>
2019-02-01 12:23:02 -0800

    Download middleware and configure server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/repos', function (req, res) {
+app.post('/repos', function(req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  console.log('Incoming POST: ', req.body);
 });
 
-app.get('/repos', function (req, res) {
+app.get('/repos', function(req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
 });
@@ -20,4 +27,3 @@ let port = 1128;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
